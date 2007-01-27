50f51dbaf66507382381149806affec8b66d98ef
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2007-01-27 04:43:25 +0000

    build.sh script working now
    git-svn-id: http://dev.adaworks.net/svn/trunk/alos@8 115cb82a-aa27-0410-86a0-a2651309cc67

--- build.sh
+++ build.sh
@@ -7,4 +7,9 @@ ADA_PROJECT_PATH=.
 for i in ../*; do ADA_PROJECT_PATH=${ADA_PROJECT_PATH}:$i; done; 
 
 
-gnatmake -P adaconfig.gpr
+
+rm src-os
+
+ln -s src-linux src-os
+
+gnatmake -gnat05 -P alos.gpr
