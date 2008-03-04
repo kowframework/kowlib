1c64b60cc815d5666d21123a39c787356b53baeb
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2008-03-04 20:26:25 +0000

    Some cleaning up:  * added function Trim() to Aw_Lib.String_Util.  * fixed Aw_Lib.String_Util identation to use tab instead of multiple spaces
    git-svn-id: http://dev.adaworks.net/svn/trunk/awlib@239 115cb82a-aa27-0410-86a0-a2651309cc67

--- src/aw_lib-string_util.adb
+++ src/aw_lib-string_util.adb
@@ -22,127 +22,149 @@ with Aw_Lib.UString_Vectors;
 with GNAT.Spitbol.Patterns;
 
 
+
 package body Aw_Lib.String_Util is
 
-   function implode( Sep: Character; Vect: Aw_Lib.UString_Vectors.Vector ) return String is
-      -- join all parts of the Vector into a String of value element1[SEP]element2[SEP]ele...
-   begin
-      return To_String( implode( Sep, Vect ) );
-   end implode;
+	function implode( Sep: Character; Vect: Aw_Lib.UString_Vectors.Vector ) return String is
+		-- join all parts of the Vector into a String of value element1[SEP]element2[SEP]ele...
+	begin
+		return To_String( implode( Sep, Vect ) );
+	end implode;
 
 
-   function implode( Sep: Character; Vect: Aw_Lib.UString_Vectors.Vector ) return Unbounded_String is
-      -- join all parts of the Vector into a Unbounded_String of value element1[SEP]element2[SEP]ele...
+	function implode( Sep: Character; Vect: Aw_Lib.UString_Vectors.Vector ) return Unbounded_String is
+		-- join all parts of the Vector into a Unbounded_String of value element1[SEP]element2[SEP]ele...
 
-      use Aw_Lib.UString_Vectors;
+		use Aw_Lib.UString_Vectors;
 
-      function implode_int ( c: Cursor ) return Unbounded_String is
-         -- recursive function to implode string
-      begin
-         return Element( c ) & Sep & implode_int( Next( c ) );
-      exception
-         when CONSTRAINT_ERROR =>
-            -- we've reached the end of the vector
-            return Element( c );
-      end;
+		function implode_int ( c: Cursor ) return Unbounded_String is
+			-- recursive function to implode string
+		begin
+			return Element( c ) & Sep & implode_int( Next( c ) );
+		exception
+			when CONSTRAINT_ERROR =>
+				-- we've reached the end of the vector
+				return Element( c );
+		end implode_int;
 
-   begin
-      return implode_int( First( Vect ) );
-   exception
-      when CONSTRAINT_ERROR =>
-         --there is nothing on this vector! :O
-         return To_Unbounded_String( "" );
-   end implode;
-
-
-   function explode( Sep: Character; Str: String ) return Aw_Lib.UString_Vectors.Vector is
-      -- split the string Str by Sep and return a vector containing it.
-   begin
-      return explode( Sep, To_Unbounded_String( Str ) );
-   end explode;
+	begin
+		return implode_int( First( Vect ) );
+	exception
+		when CONSTRAINT_ERROR =>
+			--there is nothing on this vector! :O
+			return To_Unbounded_String( "" );
+	end implode;
 
 
-   function explode( Sep: Character; Str: Unbounded_String ) return Aw_Lib.UString_Vectors.Vector is
-      -- split the string Str by Sep and return a vector containing it.
-      use Aw_Lib.UString_Vectors;
-      Vect: Vector;
-      ini, fim: Natural := 1;
-   begin
+	function explode( Sep: Character; Str: String ) return Aw_Lib.UString_Vectors.Vector is
+		-- split the string Str by Sep and return a vector containing it.
+	begin
+		return explode( Sep, To_Unbounded_String( Str ) );
+	end explode;
 
 
-      while fim <= Length( Str )
-      loop
-
-         if Element( Str, fim ) = Sep  then
-            if ini = fim then
-               Append( Vect, To_Unbounded_String( "" ) );
-            else
-               Append( Vect, Unbounded_Slice( Str, ini, fim - 1 ) );
-            end if;
-            ini := fim + 1;
-         end if;
-         fim := fim + 1;
-
-      end loop;
-
-      -- now we add the last element, it doesn't matter if it's null or not:
-      if ini = fim then
-         -- it's a null element
-         Append( Vect, To_Unbounded_String( "" ) );
-      else
-         Append( Vect, Unbounded_Slice( Str, ini, fim - 1 ) );
-      end if;
-
-      return Vect;
-
-   end explode;
+	function explode( Sep: Character; Str: Unbounded_String ) return Aw_Lib.UString_Vectors.Vector is
+		-- split the string Str by Sep and return a vector containing it.
+		use Aw_Lib.UString_Vectors;
+		Vect: Vector;
+		ini, fim: Natural := 1;
+	begin
 
 
-   procedure Str_Replace( From, To: in Character; Str: in out String ) is
-   -- replace all the ocurences of the character From by To.
-   begin
-	   for i in Str'Range loop
-		   if Str(i) = From then
-			   Str(i) := To;
-		   end if;
-	   end loop;
-   end Str_Replace;
+		while fim <= Length( Str )
+		loop
+			if Element( Str, fim ) = Sep  then
+				if ini = fim then
+					Append( Vect, To_Unbounded_String( "" ) );
+					else
+						Append( Vect, Unbounded_Slice( Str, ini, fim - 1 ) );
+				end if;
+				ini := fim + 1;
+			end if;
+			fim := fim + 1;
+		end loop;
 
-   function Str_Replace( From, To: in Character; Str: in String ) return String is
-	   -- replace all the ocurences of the character From by To returning the new Value.
-	   R: String := Str;
-   begin
-	   Str_Replace( From, To, R );
-	   return R;
-   end Str_Replace;
+		-- now we add the last element, it doesn't matter if it's null or not:
+		if ini = fim then
+			-- it's a null element
+			Append( Vect, To_Unbounded_String( "" ) );
+		else
+			Append( Vect, Unbounded_Slice( Str, ini, fim - 1 ) );
+		end if;
+
+		return Vect;
+
+	end explode;
 
 
-   procedure Str_Replace( From, To: in Unbounded_String; Str: in out Unbounded_String ) is
-      use GNAT.Spitbol.Patterns;
-      From2 : constant PString := To_String( From );
-   begin
-      Match( Str, From2, To );
-   end Str_Replace;
+	procedure Str_Replace( From, To: in Character; Str: in out String ) is
+		-- replace all the ocurences of the character From by To.
+	begin
+		for i in Str'Range loop
+			if Str(i) = From then
+				Str(i) := To;
+			end if;
+		end loop;
+	end Str_Replace;
 
-   procedure Str_Replace( From, To: in String; Str: in out Unbounded_String ) is
-   begin
-      Str_Replace( To_Unbounded_String( From ), To_Unbounded_String( To ), Str );
-   end Str_Replace;
+	function Str_Replace( From, To: in Character; Str: in String ) return String is
+		-- replace all the ocurences of the character From by To returning the new Value.
+		R: String := Str;
+	begin
+		Str_Replace( From, To, R );
+		return R;
+	end Str_Replace;
 
-   function Str_Replace( From, To: in Unbounded_String; Str: in Unbounded_String ) return Unbounded_String is
-      use GNAT.Spitbol.Patterns;
-      Replaced : Unbounded_String;
-      From2 : constant PString := To_String( From );
-   begin
-      Replaced := Str;
-      Match( Replaced, From2, To );
-      return Replaced;
-   end Str_Replace;
 
-   function Str_Replace( From, To: in String; Str: in Unbounded_String ) return Unbounded_String is
-   begin
-      return Str_Replace( To_Unbounded_String( From ), To_Unbounded_String( To ), Str );
-   end Str_Replace;
+	procedure Str_Replace( From, To: in Unbounded_String; Str: in out Unbounded_String ) is
+		use GNAT.Spitbol.Patterns;
+		From2 : constant PString := To_String( From );
+	begin
+		Match( Str, From2, To );
+	end Str_Replace;
+	
+	procedure Str_Replace( From, To: in String; Str: in out Unbounded_String ) is
+	begin
+		Str_Replace( To_Unbounded_String( From ), To_Unbounded_String( To ), Str );
+	end Str_Replace;
+
+	function Str_Replace( From, To: in Unbounded_String; Str: in Unbounded_String ) return Unbounded_String is
+		use GNAT.Spitbol.Patterns;
+		Replaced : Unbounded_String;
+		From2 : constant PString := To_String( From );
+	begin
+		Replaced := Str;
+		Match( Replaced, From2, To );
+		return Replaced;
+	end Str_Replace;
+
+	
+	function Str_Replace( From, To: in String; Str: in Unbounded_String ) return Unbounded_String is
+	begin
+		return Str_Replace( To_Unbounded_String( From ), To_Unbounded_String( To ), Str );
+	end Str_Replace;
+
+
+	function Trim( Source: in String ) return String is
+		-- removes all the white spaces in the beginning and end of the String
+		First: Integer := Source'First;
+		Last:  Integer := Source'Last;
+	begin
+		while First < Source'Last AND Source(First) = ' '
+		loop
+			First := First + 1;
+		end loop;
+
+		while First < Last and Source(Last) = ' '
+		loop
+			Last := Last - 1;
+		end loop;
+		
+		return Source( First .. Last );
+		
+	end Trim;
+
+
 
 end Aw_Lib.String_Util;
 
--- src/aw_lib-string_util.ads
+++ src/aw_lib-string_util.ads
@@ -10,48 +10,51 @@
 
 
 
-with Ada.Strings;           use Ada.Strings;
+with Ada.Strings;		use Ada.Strings;
 with Ada.Strings.Unbounded;	use Ada.Strings.Unbounded;
 
-
 with Aw_Lib.UString_Vectors;
-
 
 
 package Aw_Lib.String_Util is
 
 
-   function implode( Sep: Character; Vect: Aw_Lib.UString_Vectors.Vector ) return String;
-   -- join all parts of the Vector into a String of value element1[SEP]element2[SEP]ele...
+	function implode( Sep: Character; Vect: Aw_Lib.UString_Vectors.Vector ) return String;
+	-- join all parts of the Vector into a String of value element1[SEP]element2[SEP]ele...
 
-   function implode( Sep: Character; Vect: Aw_Lib.UString_Vectors.Vector ) return Unbounded_String;
-   -- join all parts of the Vector into a Unbounded_String of value element1[SEP]element2[SEP]ele...
+	function implode( Sep: Character; Vect: Aw_Lib.UString_Vectors.Vector ) return Unbounded_String;
+	-- join all parts of the Vector into a Unbounded_String of value element1[SEP]element2[SEP]ele...
 
-   function explode( Sep: Character; Str: String ) return Aw_Lib.UString_Vectors.Vector;
-   -- split the string Str by Sep and return a vector containing it.
+	function explode( Sep: Character; Str: String ) return Aw_Lib.UString_Vectors.Vector;
+	-- split the string Str by Sep and return a vector containing it.
 
-   function explode( Sep: Character; Str: Unbounded_String ) return Aw_Lib.UString_Vectors.Vector;
-   -- split the string Str by Sep and return a vector containing it.
-
-
-   procedure Str_Replace( From, To: in Character; Str: in out String );
-   -- replace all the ocurences of the character From by To.
-
-   function Str_Replace( From, To: in Character; Str: in String ) return String;
-   -- replace all the ocurences of the character From by To returning the new Value.
+	function explode( Sep: Character; Str: Unbounded_String ) return Aw_Lib.UString_Vectors.Vector;
+	-- split the string Str by Sep and return a vector containing it.
 
 
-   procedure Str_Replace( From, To: in Unbounded_String; Str: in out Unbounded_String );
-   -- replace all the ocurences of the character From by To.
+	procedure Str_Replace( From, To: in Character; Str: in out String );
+	-- replace all the ocurences of the character From by To.
 
-   function Str_Replace( From, To: in Unbounded_String; Str: in Unbounded_String ) return Unbounded_String;
-   -- replace all the ocurences of the character From by To returning the new Value.
+	function Str_Replace( From, To: in Character; Str: in String ) return String;
+	-- replace all the ocurences of the character From by To returning the new Value.
 
-   procedure Str_Replace( From, To: in String; Str: in out Unbounded_String );
-   -- replace all the ocurences of the character From by To.
 
-   function Str_Replace( From, To: in String; Str: in Unbounded_String ) return Unbounded_String;
-   -- replace all the ocurences of the character From by To returning the new Value.
+	procedure Str_Replace( From, To: in Unbounded_String; Str: in out Unbounded_String );
+	-- replace all the ocurences of the character From by To.
+
+	function Str_Replace( From, To: in Unbounded_String; Str: in Unbounded_String ) return Unbounded_String;
+	-- replace all the ocurences of the character From by To returning the new Value.
+
+	procedure Str_Replace( From, To: in String; Str: in out Unbounded_String );
+	-- replace all the ocurences of the character From by To.
+
+	function Str_Replace( From, To: in String; Str: in Unbounded_String ) return Unbounded_String;
+	-- replace all the ocurences of the character From by To returning the new Value.
+
+
+
+	function Trim( Source: in String ) return String;
+	-- removes all the white spaces in the beginning and end of the String
 
 end Aw_Lib.String_Util;
 
