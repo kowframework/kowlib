fe0f4b82fe9be77970cfe062c63cea53a6de1536
Marcelo C. de Freitas <marcelo@kow.com.br>
2012-01-03 21:52:40 -0200

    more changes in the build system, moving towards classic configure && make all install

--- Makefile
+++ Makefile
@@ -5,28 +5,12 @@
 #
 # Please, read Makefile.include for more information
 
-PROJECT=kowlib
-INCLUDE_FILES=src/*
 
-ENABLE_DEBUG=false
+all:
+	./build
 
-GPRBUILD_PARAMS=-XKOWLIB_EXTERNALLY_BUILT=false
+install:
+	./install
 
-
-ifeq ($(OS), Windows_NT)
-	INCLUDE_FILES=src/* src-windows/*
-else
-	INCLUDE_FILES=src/* src-posix/*
-endif
-
-include Makefile.include
-
-
-pre_setup:
-extra_gnatprep_setup:
-pos_setup:
-pre_build:
-pos_build:
-
-extra_clean:
-
+uninstall:
+	./uninstall
--- scripts/configutil.sh
+++ scripts/configutil.sh
@@ -1,10 +1,72 @@
 #!/usr/bin/env bash
+# 
+# Auxiliary functions for the KOW Framework main build system
+#
+# @author Marcelo C. de Freitas
 
 
 
+# Check if a given command is in path;
+# usage:
+#	check_in_path COMMAND_NAME
 check_in_path(){
 	echo -n "Looking for $1	... "
 	hash $1 2>&- || { echo "[false]"; echo >&2 "I need $1 in path but I can't find it... aborting"; exit 1; } && echo "[ok]"
 }
 
 
+
+# Copy updating a regular file or directory
+# usage:
+#	cpu origin destination
+cpu(){
+	if [[ -f "$1" ]]
+	then
+		if [[ "$1" -nt "$2" ]]
+		then
+			cp "$1" "$2";
+		else
+			echo "Not copying \"$1\""
+		fi
+	else
+		echo "Can't copy (not a regular file): \"$1\""
+		exit 1;
+	fi
+}
+
+
+
+############################
+# Configuration Management #
+############################
+
+
+# reset the configuration file
+init_configuration(){
+	echo -n "" > .configuration
+}
+
+
+
+# Set a configuration key/value:
+# usage:
+#	setconfiguration key value
+set_configuration() {
+	echo $1=\"$2\" >> .configuration
+}
+
+
+# Printout the configuration file
+# usage:
+#	cat_configuration
+cat_configuration(){
+	cat .configuration
+}
+
+
+# load the configuration file:
+# usage:
+#	load_configuration
+load_configuration(){
+	source .configuration
+}
